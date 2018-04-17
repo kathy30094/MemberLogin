36ba2d7bac59a0315c490281628c2c951a3607c6
kathy30094 <kathy30094@gmail.com>
2018-04-17 17:31:09 +0800

    test3

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -107,29 +107,6 @@ export default {
             });
             this.isChange = 'unchanged';
         },
-        // checkCSRFTOKEN(){
-        //     var XSRFTOKEN = this.getCookie('XSRF-TOKEN');
-        //     console.log(XSRFTOKEN);
-        //     if(XSRFTOKEN == "")
-        //     {
-        //         window.location.reload();
-        //     }
-        // },
-        // getCookie(cname) {
-        //     var name = cname + "=";
-        //     var decodedCookie = decodeURIComponent(document.cookie);
-        //     var ca = decodedCookie.split(';');
-        //     for(var i = 0; i <ca.length; i++) {
-        //         var c = ca[i];
-        //         while (c.charAt(0) == ' ') {
-        //             c = c.substring(1);
-        //         }
-        //         if (c.indexOf(name) == 0) {
-        //             return c.substring(name.length, c.length);
-        //         }
-        //     }
-        //     return "";
-        // }
     },
 
     mounted() {
